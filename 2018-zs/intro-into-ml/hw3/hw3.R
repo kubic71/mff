6d519ee7b6fede93c7db2ff2d13edab08b736532
Jakub Hejhal <kubic71@gmail.com>
2019-01-07 00:26:10 +0100

    part 3 and 4 almost done

--- 2018-zs/intro-into-ml/hw3/hw3.R
+++ 2018-zs/intro-into-ml/hw3/hw3.R
@@ -3,12 +3,50 @@ library(FSelector)
 library(rpart)
 library(rpart.plot)
 library(ROCR)
+library(randomForest)
 
 # load development data
 d1 = fread("devel1.csv")
 d2 = fread("devel2.csv")
 d = rbind(d1, d2)
 t.blind = fread("test.blind.csv")
+
+#####################################
+######### some functions ############
+#####################################
+
+# get stratified folds from data
+get_folds = function(data, k) {
+  #shuffle data
+  data = data[sample(nrow(data)),]
+  
+  positive = data[data$active == 1 , ]
+  negative = data[data$active == 0 , ]
+  
+  pos.folds = cut(seq(1,nrow(positive)),breaks=k,labels=FALSE)
+  neg.folds = cut(seq(1,nrow(negative)),breaks=k,labels=FALSE)
+  
+  #list initialization
+  folds = vector("list", k)
+  
+  for(i in 1:k) {
+    pos.fold = positive[which(pos.folds==i,arr.ind=TRUE), ]
+    neg.fold = negative[which(neg.folds==i,arr.ind=TRUE), ]
+    folds[[i]] = rbind(pos.fold, neg.fold)
+  }
+  
+  return(folds)
+}
+
+get_auc01 = function(pred) {
+  auc01 = performance(pred, measure = "auc", fpr.stop=0.1)
+  return(as.numeric(auc01@y.values))
+}
+
+#####################################
+#####################################
+
+
 
 
 #####################################
@@ -129,33 +167,6 @@ message("\n\n\n--- task 2b ---")
 set.seed(1)
 
 
-# get stratified folds from data
-get_folds = function(data, k) {
-  #shuffle data
-  data = data[sample(nrow(data)),]
-  
-  positive = data[data$active == 1 , ]
-  negative = data[data$active == 0 , ]
-  
-  pos.folds = cut(seq(1,nrow(positive)),breaks=k,labels=FALSE)
-  neg.folds = cut(seq(1,nrow(negative)),breaks=k,labels=FALSE)
-  
-  #list initialization
-  folds = vector("list", k)
-  
-  for(i in 1:k) {
-    pos.fold = positive[which(pos.folds==i,arr.ind=TRUE), ]
-    neg.fold = negative[which(neg.folds==i,arr.ind=TRUE), ]
-    folds[[i]] = rbind(pos.fold, neg.fold)
-  }
-  
-  return(folds)
-}
-
-get_auc01 = function(pred) {
-  auc01 = performance(pred, measure = "auc", fpr.stop=0.1)
-  return(as.numeric(auc01@y.values))
-}
 
 # k-fold cross-validation
 cv = function(data, k=10, cp=NULL) {
@@ -251,4 +262,129 @@ message("\n\n\n\n\n\n")
 message("##############################")
 message("----------- TASK 3 -----------")
 message("##############################")
+
+# return auc01 for given train and test data and given hyperparameters
+evaluate_rf = function(train, test, ntree, mtry) {
+  fit = randomForest(active ~ .,  train, ntree=ntree, mtry=mtry)
+  prob = predict(fit, newdata=test)
+  pred = prediction(prob, test$active)
+  return(get_auc01(pred))
+}
+
+
+plot(c(), c(), xlab="mtry", ylab="auc01 on test set (D2)", main="Dependency of AUC01 on ntree and mtry", xlim = c(0,30), ylim=c(0, 0.1))
+
+for (ntree in c(500)) {
+  auc01s = c()
+  
+  mtries = c(1, 2, 4, 8, 16, 25)
+  for(mtry in mtries) {
+    auc01.train = evaluate_rf(d1, d1, ntree, mtry)
+    auc01.test = evaluate_rf(d1, d2, ntree, mtry)
+    auc01s = c(auc01s, auc01.test)
+    message("mtry:\t", mtry, "\tntree:\t", ntree, "\tauc01.train:\t", round(auc01.train, 5), "\tauc01.test\t", round(auc01.test, 5))
+    
+  }
+  
+  points(mtries, auc01s, pch=16)
+  lines(mtries,  auc01s, pch=16)
+  
+}
+
+
+
+
+
+#####################################
+#-------------- TASK 4 --------------
+#####################################
+
+
+message("\n\n\n\n\n\n")
+message("##############################")
+message("----------- TASK 4 -----------")
+message("##############################")
+
+
+# --- task 4a ---
+message("\n\n\n--- task 4a ---")
+### training - 4/5 D1
+### testing  - 1/5 D1
+
+
+ntree = 1000
+mtry = 10
+
+# 5-fold cross-validation
+cv_rf = function(const_data, var_data, flip_train_test = FALSE) {
+  message("ntree: ", ntree, "\tmtry: ", mtry)
+  
+  folds = get_folds(var_data, 5)
+  auc = c()
+  
+  for(i in 1:5) {
+    d.test = as.data.frame(folds[i])
+    d.train = do.call("rbind", folds[-c(i)])
+    
+    # for the 4b task
+    if (flip_train_test) {
+      temp = d.test
+      d.test = d.train
+      d.train = temp
+    }
+    
+    d.train = rbind(const_data, d.train)
+    
+    fit = randomForest(active ~ .,  d.train, ntree=ntree, mtry=mtry)
+    prob = predict(fit, newdata=d.test)
+    pred = prediction(prob, d.test$active)
+    auc = c(auc, get_auc01(pred))    
+    #message("iteration: ", i, ", auc01: ", auc01)
+  } 
+  return(auc)
+  
+}
+
+report_mean_auc01 = function(auc01.vec) {
+  auc01 = t.test(auc01.vec, conf.level=0.95)
+  message("AUC0.1 mean estimate:\t", round(auc01$estimate, 5))
+  message("Standard deviation:\t", round(sd(auc01.vec), 6))
+  message("Confidence interval:\t", round(auc01$conf.int[1], 5), ", ", round(auc01$conf.int[2], 5))
+  
+}
+
+
+auc01.vec = cv_rf(NULL, d1)
+report_mean_auc01(auc01.vec)
+
+# performance on D2
+fit = randomForest(active ~ .,  d1, ntree=ntree, mtry=mtry)
+prob = predict(fit, newdata=d2)
+pred = prediction(prob, d2$active)
+message("performance on whole D2: ", round(get_auc01(pred), 5))
+
+
+
+# --- task 4b ---
+message("\n\n\n--- task 4b ---")
+
+auc01.vec = cv_rf(d1, d2, TRUE)
+report_mean_auc01(auc01.vec)
+
+
+
+# --- tak 4c ---
+message("\n\n\n--- task 4c ---")
+
+auc01.vec = cv_rf(d1, d2)
+report_mean_auc01(auc01.vec)
+
+
+
+# --- tak 4d ---
+message("\n\n\n--- task 4d ---")
+
+auc01.vec = cv_rf(NULL, d2)
+report_mean_auc01(auc01.vec)
+
 
