1028b07887e8b65106e29fffa6bacc2937873a51
Jakub Hejhal <kubic71@gmail.com>
2020-10-01 21:23:00 +0200

    datovky hw1 done

---
+++ 2020-zs/datovky/hw/01-tree_successor/python/tree_successor.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+class Node:
+    """Node in a binary tree `Tree`"""
+
+    def __init__(self, key, left=None, right=None, parent=None):
+        self.key = key
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+class Tree:
+    """A simple binary search tree"""
+
+    def __init__(self, root=None):
+        self.root = root
+        
+    def insert(self, key):
+        """Insert key into the tree.
+
+        If the key is already present, do nothing.
+        """
+        if self.root is None:
+            self.root = Node(key)
+            return
+
+        node = self.root
+        while node.key != key:
+            if key < node.key:
+                if node.left is None:
+                    node.left = Node(key, parent=node)
+                node = node.left
+            else:
+                if node.right is None:
+                    node.right = Node(key, parent=node)
+                node = node.right
+
+    def successor(self, node=None):
+        """Return successor of the given node.
+
+        The successor of a node is the node with the next greater key.
+        Return None if there is no such node.
+        If the argument is None, return the node with the smallest key.
+        """
+        if node is None:
+            node = self.root
+            while node.left is not None:
+                node = node.left
+            return node
+
+        # return minimum of right subtree, if it exist
+        if node.right is not None:
+            return Tree(node.right).successor()
+        
+        # if node doesn't have right subtree, it is already a maximum in some subtree
+        # the successor therefore is the parent node of this subtree
+        val = node.key
+        while node.key <= val:
+            node = node.parent
+            if node is None:
+                return None
+            
+        return node
