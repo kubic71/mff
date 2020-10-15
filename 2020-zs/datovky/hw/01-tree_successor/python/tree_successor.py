305aeadbc479f909e14783a070633e68b1e38036
Jakub Hejhal <kubic71@gmail.com>
2020-10-15 03:12:44 +0200

    datovky hw1 fix

--- 2020-zs/datovky/hw/01-tree_successor/python/tree_successor.py
+++ 2020-zs/datovky/hw/01-tree_successor/python/tree_successor.py
@@ -34,6 +34,21 @@ class Tree:
                 if node.right is None:
                     node.right = Node(key, parent=node)
                 node = node.right
+                
+#    def _find_min(self, node):
+#        # find min in subtree rooted at node
+#        if node.left is None:
+#            return node
+#
+#        return self._find_min(node.left)
+                
+    def _find_min(self, node):
+        while node.left is not None:
+            node = node.left
+        return node
+            
+
+        
 
     def successor(self, node=None):
         """Return successor of the given node.
@@ -42,22 +57,24 @@ class Tree:
         Return None if there is no such node.
         If the argument is None, return the node with the smallest key.
         """
+
         if node is None:
-            node = self.root
-            while node.left is not None:
-                node = node.left
-            return node
+            return self._find_min(self.root)
+            
 
         # return minimum of right subtree, if it exist
         if node.right is not None:
-            return Tree(node.right).successor()
+            return self._find_min(node.right)
         
         # if node doesn't have right subtree, it is already a maximum in some subtree
         # the successor therefore is the parent node of this subtree
-        val = node.key
-        while node.key <= val:
-            node = node.parent
-            if node is None:
-                return None
+
+        while node.parent is not None:
             
-        return node
+            # if node is the left son of its parent 
+            if node.parent.left is node:
+                return node.parent
+                
+            node = node.parent
+            
+        return None
