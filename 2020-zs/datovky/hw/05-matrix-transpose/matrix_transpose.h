47a2c94b8a1641766514fdc9165923c2e75db61a
Jakub Hejhal <kubic71@gmail.com>
2020-11-09 04:36:07 +0100

    ds hw5 refactoring

--- 2020-zs/datovky/hw/05-matrix-transpose/matrix_transpose.h
+++ 2020-zs/datovky/hw/05-matrix-transpose/matrix_transpose.h
@@ -19,7 +19,7 @@
 // unsigned N = 0;
 // void swap(unsigned i1, unsigned j1, unsigned i2, unsigned j2);
 
-void transpose_and_swap(unsigned i1, unsigned j1, unsigned w1, unsigned h1, unsigned i2, unsigned j2, unsigned w2, unsigned h2) {
+void transpose_and_swap(unsigned i1, unsigned j1, unsigned i2, unsigned j2, unsigned w1, unsigned h1) {
     
     if(w1 == 0 || h1 == 0)
         return;
@@ -28,40 +28,39 @@ void transpose_and_swap(unsigned i1, unsigned j1, unsigned w1, unsigned h1, unsi
         return;
     }
 
-
+    unsigned w2 = h1;
+    unsigned h2 = w1;
+    
     // A_1,1 <-> B_1,1
-    transpose_and_swap(i1       , j1       , w1/2     , h1/2     , i2       , j2       , w2/2     , h2/2     );
+    transpose_and_swap(i1       , j1       , i2       , j2       , w1/2     , h1/2     );
 
     // A_2,2 <-> B_2,2
-    transpose_and_swap(i1 + h1/2, j1 + w1/2, w1 - w1/2, h1 - h1/2, i2 + h2/2, j2 + w2/2, w2 - w2/2, h2 - h2/2);
+    transpose_and_swap(i1 + h1/2, j1 + w1/2, i2 + h2/2, j2 + w2/2, w1 - w1/2, h1 - h1/2);
 
-    // A_2,1 <-> B_1,2
-    transpose_and_swap(i1 + h1/2, j1       , w1/2     , h1 - h1/2, i2       , j2 + w2/2, w2 - w2/2, h2/2     );
+    // A_2,1 <-> B_1,2                                                                            
+    transpose_and_swap(i1 + h1/2, j1       , i2       , j2 + w2/2, w1/2     , h1 - h1/2);
 
-    // A_1,2 <-> B_2,1
-    transpose_and_swap(i1       , j1 + w1/2, w1 - w1/2, h1/2     , i2 + h2/2, j2       , w2/2     , h2 - h2/2);
+    // A_1,2 <-> B_2,1                                                                        
+    transpose_and_swap(i1       , j1 + w1/2, i2 + h2/2, j2       , w1 - w1/2, h1/2     );
 
 }
 
 
-// transpose square submatrix
+// transpose square submatrix starting at (i, j) with size n*n
 void transpose(unsigned i, unsigned j, unsigned n) {
     if(n < 2)
         return;
 
-    transpose(i, j, n/2);
-    transpose(i + n/2 , j + n/2, n - n/2);
+    int s1 = n/2;
+    int s2 = n - s1;
+
+    transpose(i, j, s1);
+    transpose(i + s1 , j + s1, s2);
     
-    transpose_and_swap(i + n/2, j, n/2, n - n/2, i, j + n/2, n - n/2, n/2);
+    transpose_and_swap(i + s1, j, i, j + s1, s1, s2);
 }
 
 void transpose()
 {
     transpose(0, 0, N);
-
-    // TODO: Implement this efficiently 
-
-    // for (unsigned i=0; i<N; i++)
-    //     for (unsigned j=0; j<i; j++)
-    //         swap(i, j, j, i);
 }
