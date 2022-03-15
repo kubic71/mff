539881d51e2c45d51a0bb01b847c68e113865a59
Jakub Hejhal <kubic71@gmail.com>
2022-03-15 19:17:36 +0100

    finishing hw2

---
+++ 2021-zs/stat_nlp/hw_2/word_classes.md
@@ -0,0 +1,168 @@
+# Word Classes
+## The data
+Get **TEXTEN1.ptg** and **TEXTCZ1.ptg**
+
+These are your data. They are almost the same as the .txt data you have used so far, except they now contain the part of speech tags in the following form:
+```
+rady/NNFS2-----A----
+,/Z:-------------
+```
+
+where the tag is separated from the word by a slash (`'/'`). Be careful: the tags might contain everything (including slashes, dollar signs and other weird characters). It is guaranteed however that there is no slash-word.
+
+Similarly for the English texts (except the tags are shorter of course).
+
+## The Task
+Compute a full class hierarchy of words using the first 8,000 words of those data, and only for words occurring 10 times or more (use the same setting for both languages). Ignore the other words for building the classes, but keep them in the data for the bigram counts and all the formulas that use them (including the Mutual Information, the interim sums in the "Tricks", etc.). For details on the algorithm, use the Brown et al. paper available form SIS; some formulas are wrong in the paper, however, so please see the corrections in the slides (formulas for Trick #4). Note the history of the merges, and attach it to your homework. Now run the same algorithm again, but stop when reaching 15 classes. Print out all the members of your 15 classes and attach them too.
+
+## Hints
+The initial mutual information is (English, words, limit 8000):
+
+`4.99726326162518` (if you add one extra word at the beginning of the data)
+`4.99633675507535` (if you use the data as they are and are carefull at the beginning and end).
+
+NB: the above numbers are finally confirmed from an independent source :-).
+
+The first 5 merges you get on the English data should be:
+```
+case subject
+cannot may
+individuals structure
+It there
+even less
+```
+
+The loss of Mutual Information when merging the words `case` and `subject`:
+
+`Minimal loss: 0.00219656653357569 for case+subject`
+
+
+## Solution
+### Algorithm
+I implented the $O(V^3)$ version of the algorithm, incorporating most of the tricks, except the **skip-zero-bigrams** one.
+It runs reasonably fast and it was enough for this assignment.
+
+
+### Full history of word class merges
+The full history for both datasets is computed by running the following commands:
+
+```shell
+$ python word_classes.py --dataset_path datasets/TEXTEN1.ptg --target_num_classes 1 --N 8000
+$ python word_classes.py --dataset_path datasets/TEXTCZ1.ptg --target_num_classes 1 --N 8000 
+```
+
+The merge histories are exported to the `results` folder:
+- [merge_results_words_CZ_N-8000_target_num-1.txt](results/merge_results_words_CZ_N-8000_target_num-1.txt) for Czech 
+- [merge_results_words_EN_N-8000_target_num-1.txt](results/merge_results_words_EN_N-8000_target_num-1.txt) for English
+
+
+#### Correctness
+The first five merges for English with 8000 word limit are:
+```
+|112. I(D, E): 4.997263261625193, loss: 0.0021965665335752504, merging:
+subject
+case
+
+|111. I(D, E): 4.995066695091618, loss: 0.002669139511099594, merging:
+may
+cannot
+
+|110. I(D, E): 4.992397555580518, loss: 0.0026748091526132, merging:
+individuals
+structure
+
+|109. I(D, E): 4.989722746427905, loss: 0.003479400370452801, merging:
+It
+there
+
+|108. I(D, E): 4.986243346057452, loss: 0.0036556390622299872, merging:
+less
+even
+```
+
+The computed initial mutual information `I(D,E) = 4.997263261625193` is the same as the one in the **Hint** (`4.99726326162518`) up to 14 decimal places.
+
+The computed loss of the first merge (`case` and `subject`) is  `0.0021965665335752504`, which equals the **Hint** (`0.00219656653357569`) up to 15 decimal places.
+
+The first 5 merges also agree with the hint.
+So I believe my implementation is correct.
+
+
+### Stopping at 15 classes 
+We run the `word_classes.py` again, but this time with `--target_num_classes 15` parameter.
+
+```shell
+$ python word_classes.py --dataset_path datasets/TEXTEN1.ptg --target_num_classes 15 --N 8000
+$ python word_classes.py --dataset_path datasets/TEXTCZ1.ptg --target_num_classes 15 --N 8000 
+```
+
+The merge histories with the final 15 classes are again in the `results`:
+- [merge_results_words_CZ_N-8000_target_num-15.txt](results/merge_results_words_CZ_N-8000_target_num-15.txt) for Czech
+- [merge_results_words_EN_N-8000_target_num-15.txt](results/merge_results_words_EN_N-8000_target_num-15.txt) for English.
+
+
+#### Final 15 Czech classes
+Each class is on seperate line with the words separated by spaces.
+```
+,
+a
+o
+před byl ? k po si při včera
+nás státu J to jeho zákona NATO &slash; ČSFR jsou )
+ve pro u za ale aby který které že
+v
+na
+s Na OKD listopadu V "
+i bylo jako však musí bude být budou pouze by
+už mezi od ze : (
+-
+se
+je z do
+.
+```
+
+#### Final 15 English classes
+```
+.
+its an wild same been
+all some these each one distinct certain what
+it they will we can there It must
+of
+in
+the
+by with at on has or is that
+more under for domesticated domestic long slight short as which this
+varieties races facts only me plants animals ; individuals structure most nature variation breeds state subject case conditions our several many
+,
+and
+to my different their great ( any very such manner a
+be I have from : between nearly how In The cannot may could would
+are do see shall believe when if even less other not so but ) cases than differ much often species
+```
+
+
+# Tag classes
+Use the same original data as above, but this time, you will compute the classes for tags (the strings after slashes). Compute tag classes for all tags appearing 5 times or more in the data. Use as much data as time allows. You will be graded relative to the other student's results. Again, note the full history of merges, and attach it to your homework. Pick three interesting classes as the algorithm goes (English data only; Czech optional), and comment on them (why you think you see those tags there together (or not), etc.).
+
+## Solution
+The classes for tags were computed on **all** given data, without any limit.
+
+```shell
+$ python word_classes.py --dataset_path datasets/TEXTCZ1.ptg --target_num_classes 1 --mode tags
+$ python word_classes.py --dataset_path datasets/TEXTEN1.ptg --target_num_classes 1 --mode tags
+```
+
+Tag merge histories are again in the `results` directory:
+- [merge_results_tags_CZ_N-224538_target_num-1.txt](results/merge_results_tags_CZ_N-224538_target_num-1.txt) for Czech
+- [merge_results_tags_EN_N-221098_target_num-1.txt](results/merge_results_tags_EN_N-221098_target_num-1.txt) for English
+
+
+### Three interesting merges
+1. VBD (Verb, past tense) + VBN (Verb, past participle)
+   - It makes sense to merge verbs that talk about the past, either in ordinary past tense or in past participle, into one general _Verb, past_ class
+2. NN (Noun, singular or mass) + NNPS (Proper noun, plural)
+   - Merging together different types of nouns
+3. PRP$ (Possesive pronoun) + DT (Determiner)
+   - The possesive pronoun can be often exchanged with determiner, while keeping the sentence grammatically correct
+     - `My house` vs. `The house`
+
--- 2021-zs/stat_nlp/hw_2/word_classes.py
+++ 2021-zs/stat_nlp/hw_2/word_classes.py
@@ -1,7 +1,10 @@
 from collections import defaultdict
-from email.policy import default
 import itertools
 import math
+from time import clock_settime
+from matplotlib.pyplot import cla
+import tqdm
+import os
 
 def load_data(path):
     with open(path) as file:
@@ -15,62 +18,18 @@ def load_data(path):
     
     return words, tags
 
-def get_unigram_counts(data):
-    unigrams = defaultdict(lambda: 0)
-    for word in data:
-        unigrams[word] += 1
-    return unigrams
 
+def init_bigram_and_unigram_counts(words):
+    c_l = defaultdict(lambda: 0)
+    c_r = defaultdict(lambda: 0)
+    c = defaultdict(lambda: 0)
 
-def get_bigram_probs(data):
-    """
-    Compute bigram probabilities.
-    
-    data: list of words
-    N: total number of words
-    filter: function to filter out words
-    """
+    for i in range(len(words) - 1):
+        c_l[words[i]] += 1
+        c_r[words[i+1]] += 1
+        c[words[i], words[i+1]] += 1
 
-    bigrams = defaultdict(lambda: 0)
-    for i in range(len(data) - 1):
-        # TODO: is this correct? 
-        # Refering to the assignment:
-        # >  Ignore the other words for building the classes, 
-        # > but keep them in the data for the bigram counts and 
-        # > all the formulas that use them (including the Mutual Information, 
-        # > the interim sums in the "Tricks", etc.). 
-
-        bigrams[data[i], data[i+1]] += 1
-    
-    return normalize(bigrams)
-
-
-def normalize(bigrams):
-    """Normalize bigram probabilities, so they sum to 1."""
-    N = sum(bigrams.values())
-    normalized = defaultdict(lambda: 0)
-
-    for bigram, count in bigrams.items():
-        normalized[bigram] = count / N
-
-    return normalized
-
-
-def init_unigram_probs(bigrams, left=True):
-    """Marginal unigram probabilities from bigram probabilities."""
-    unigrams = defaultdict(lambda: 0)
-
-    for bigram, prob in bigrams.items():
-        l, r = bigram
-        if left:
-            unigrams[l] += prob
-        else:
-            unigrams[r] += prob
-
-    # check, if all unigrams sum to 1 (reasonably close)
-    # assert abs(sum(unigrams.values()) - 1) < 0.001
-
-    return unigrams
+    return c_l, c_r, c
 
 
 def cartesian_sum(matrix, rows, cols):
@@ -81,226 +40,329 @@ def cartesian_sum(matrix, rows, cols):
     rows: rows of q
     cols: cols of q
     """
+
     res = 0 
     for row in rows:
         for col in cols:
             res += matrix[row, col]
     return res
 
-def sub(s, q, a, b):
-    return s[a] + s[b] - q[a, b] - q[b, a]
-
-def pmi(prob_ab, prob_a, prob_b):
-    # print(prob_ab, prob_a, prob_b)
-    if prob_ab == 0:
-        return 0
-
-    res = prob_ab * (math.log2(prob_ab) - math.log2(prob_a) - math.log2(prob_b))
-    return res
-
-def q_macro(bi_prob, uni_l, uni_r, rows, cols):
-    """
-        Compute q(rows, cols) from bigram and marginal unigram probabilities as if rows and cols were merged.   
-    """
-
-    # compute the merged bigram probability
-    p_r_c = cartesian_sum(bi_prob, rows, cols)
-
-    # compute the merged unigram probabilities
-    # TODO: maybe swap this
-    p_row = sum(uni_l[row] for row in rows)
-    p_col = sum(uni_r[col] for col in cols)
-
-    return pmi(p_r_c, p_row, p_col)
-
-
-def add(bi_prob, p_l, p_r, classes, a, b):
-    # notation: a+b means union of a and b (as if a and b were merged)
-    # we need to add:
-    #  q(x, a+b), x not in [a, b]
-    #  q(a+b, y), y not in [a, b]
-    #  q(a+b, a+b)
-
-    res = 0
-
-    merged = {a, b}
-    new_classes = classes - merged
-
-    for c in new_classes:
-        res += q_macro(bi_prob, p_l, p_r, merged, {c})
-        res += q_macro(bi_prob, p_l, p_r, {c}, merged)
-
-    res += q_macro(bi_prob, p_l, p_r, merged, merged)
-
-    return res
-
-
 def merge(a, b):
     return a + "/" + b
 
-def update_s(s, q, q_new, a, b, classes):
-    """
-        s, q are from the previous iteration
-        a, b are classes to be merged
-        classes is the set of classes after the merge
-    """
-    raise NotImplementedError
-    new_s = defaultdict(lambda: 0)
-    for i in classes:
-        pass
-        # new_s[i] = s[i] - q[i, a] - q[a, i] - q[i, b] - q[b, i] + 
+def compute_word_classes(data, target_num_classes, output_fn, min_word_count=10, N=8000):
+    
+    output_file = open(output_fn, "w")
 
-def update_q(q, a, b, classes):
-    # merging a and b
-    raise NotImplementedError
-
-
-def all_bigram_classes(bigrams):
-    all_words = set()
-    for l, r in bigrams.keys():
-        all_words.add(l)
-        all_words.add(r)
-    return all_words
-
-
-def compute_word_classes(data, target_num_classes, min_word_count=10, dataset_cutoff=None):
+    def print_and_write(string):
+        print(string)
+        output_file.write(string + "\n")
+        output_file.flush()
 
     # Words, that appear in the data at least min_word_count times
     # If dataset_cutoff is not None, take only first dataset_cutoff words
-    words_considered = []
-    if dataset_cutoff is not None:
-        data = data[:dataset_cutoff]
+
+    assert N <= len(data)
+    data = data[:N] 
 
     # add special token to the beginning of the data
-    data = ["<s>"] + data
+    special_token = "<s>"
+    data = [special_token] + data
 
-    word_counts = get_unigram_counts(data)
-    words_considered = {word for word in word_counts if word_counts[word] >= min_word_count}
 
-    print("Number of words considered:", len(words_considered))
-    bigrams = get_bigram_probs(data)
+    c_l, c_r, c = init_bigram_and_unigram_counts(data)
+    
+    classes = {word for word in c_r if c_r[word] >= min_word_count}
+    print_and_write(f"{len(classes)} considered classes: {' '.join(sorted(classes))}\n")
 
-    # check, that the number of bigrams is the square of the number of words considered
-    print("Number of unique bigrams:", len(bigrams))
 
-    classes = words_considered.copy()
-    # TODO: maybe it's okay to disregard the ends of the dataset and consider p_l == p_r
-    p_l = init_unigram_probs(bigrams, left=True)
-    p_r = init_unigram_probs(bigrams, left=False)
+    all_classes = set(c_r.keys())
+    all_classes.add(special_token)
 
-    print("Number of unique words in bigrams:", len(all_bigram_classes(bigrams)))
+
+    def pmi(ab_counts, counts_a, counts_b):
+        if ab_counts == 0:
+            return 0
+        return  ab_counts / N * math.log2(N * ab_counts / (counts_a * counts_b))
+
+    def q_macro(rows, cols):
+        """
+            Compute q(rows, cols) from bigram and marginal unigram probabilities as if rows and cols were merged.   
+        """
+
+        # compute the merged bigram probability
+        p_r_c = cartesian_sum(c, rows, cols)
+
+        # compute the merged unigram probabilities
+        # TODO: maybe swap this
+        p_row = sum(c_l[row] for row in rows)
+        p_col = sum(c_r[col] for col in cols)
+
+        return pmi(p_r_c, p_row, p_col)
+
+
+    def add(all_classes, a, b):
+        # notation: a+b means union of a and b (as if a and b were merged)
+        # we need to add:
+        #  q(x, a+b), x not in [a, b]
+        #  q(a+b, y), y not in [a, b]
+        #  q(a+b, a+b)
+
+        res = 0
+
+        merged = (a, b)
+
+        for c in all_classes:
+            if c == a or c == b:
+                continue
+            res += q_macro(merged, (c, ))
+            res += q_macro((c,), merged)
+
+        res += q_macro(merged, merged)
+
+        return res
+
+    def sub(a, b, q, s):
+        return s[a] + s[b] - q[a, b] - q[b, a]
+
+    # init q
+    print("Initializing q...")
+    q = {}
+    for l in all_classes:
+        for r in all_classes:
+            q[l, r] = pmi(c[l, r], c_l[l], c_r[r])
+
+    # init s
+    print("Initializing s...")
+    s = {}
+    for a in all_classes:
+        s[a] = 0
+        for w in all_classes:
+            s[a] += q[w, a] + q[a, w]
+        s[a] -= q[a, a]
+
+    # for a, b in c:
+    #     s[a] += q[b, a]
+    #     s[a] += q[a, b]   
+    
+    # for a in all_classes:
+    #     s[a] -= q[a, a]
+
+
+    def L_get(L, a, b):
+        if (a, b) in L:
+            return L[a, b]
+        else:
+            return L[b, a]
+
+    # init L
+    print("Initializing L...")
+    L = {}
+    for a, b in tqdm.tqdm(list(itertools.combinations(classes, 2))):
+        L[a, b] = sub(a, b, q, s) - add(all_classes, a, b)
+    
+
+    def update_counts(a, b, classes):
+
+        ab = merge(a, b)
+        c_l[ab] = c_l[a] + c_l[b]
+        c_r[ab] = c_r[a] + c_r[b]
+
+
+        for cl in classes:
+            c[cl, ab] = c[cl, a] + c[cl, b]
+            c[ab, cl] = c[a, cl] + c[b, cl]
+        
+        c[ab, ab] = c[a, a] + c[a, b] + c[b, a] + c[b, b]
+
+
+    
+    def update_q(q, a, b, classes):
+        ab = merge(a, b)
+        q2 = q.copy()
+
+        for i, j in q:
+            if i == a or i == b or j == a or j == b:
+                del q2[i, j]
+
+        # add q(x, a+b) for x not in [a, b]
+
+        for i in classes:
+            if i == a or i == b:
+                continue
+            q2[i, ab] = pmi(c[i, ab], c_l[i], c_r[ab])
+            q2[ab, i] = pmi(c[ab, i], c_r[i], c_l[ab])
+
+        q2[ab, ab] = pmi(c[ab, ab], c_r[ab], c_l[ab])
+        
+        return q2
+
+
+    def update_s(s, q, q2, a, b, classes):
+        
+        ab = merge(a, b)
+        s2 = defaultdict(lambda: 0)
+
+        for i in classes:
+            if i == a or i == b:
+                continue
+              
+            s2[i] = s[i] - q[i, a] - q[a, i] - q[i, b] - q[b, i] + q2[ab, i] + q2[i, ab]
+
+
+        # s2(ab) must be computed using the "Init" sum
+        for i in classes:
+            if i == a or i == b:
+                continue
+
+            assert i != ab
+            s2[ab] += q2[i, ab] + q2[ab, i]
+        
+        s2[ab] += q2[ab, ab]
+
+        return s2
+
+    def update_L(L, q, q2, s, s2, a, b, classes, new_all_classes):
+        ab = merge(a, b)
+
+        L2 = {}
+        for i, j in itertools.combinations(classes, 2):
+            if i == a or i == b or j == a or j == b:
+                continue
+
+            L2[i, j] = L_get(L, i, j) - s[i] + s2[i] - s[j] + s2[j]
+            L2[i, j] += q_macro((i, j), (a,)) + q_macro((a,), (i, j)) + q_macro((i, j), (b,)) + q_macro((b,), (i, j))
+            L2[i, j] -= q_macro((i, j), (ab,)) - q_macro((ab,), (i, j))
+
+        # L2[ab, i] and L2[i, ab] must be computed using the "Init" sum
+
+        for i in classes:
+            if i == a or i == b:
+                continue
+
+            assert i != ab
+            L2[ab, i] = sub(ab, i, q2, s2) - add(new_all_classes, ab, i)
+
+        return L2
+
+    def update_classes(classes, a, b):
+        new_c = classes.copy()
+        new_c.remove(a)
+        new_c.remove(b)
+        new_c.add(merge(a, b))
+
+        return new_c
+
+
+
+    print("Initializing I")
+    I = 0
+    for l in all_classes:
+        for b in all_classes:
+            I += q[l, b]
 
     while len(classes) > target_num_classes:
+        # print(len(classes), len(all_classes))
 
-        # init q(l, r), which are the probability-weighted point-wise mutual informations
-        # TODO: maybe we want to compute q(l, r) from counts instead of probabilities for better numerical stability
-        q = defaultdict(lambda: 0)
-        for bigram, prob in bigrams.items():
-            l, r = bigram
-            q[l, r] = pmi(prob, p_l[l], p_r[r])
-
-        # compute the sum of the q(l, r) values, i.e. the mutual information I(D,E)
-        I = sum(q.values())
-        print("before merge I(D, E):", I)
-
-        # pre-compute the sub values
-        # s[k] ... sum of row q(*, k) and column q(k, *)
-        s = {}
-
-        all_classes = all_bigram_classes(bigrams)
-        for k in all_classes:
-            s[k] = cartesian_sum(q, rows=(k, ), cols=all_classes)
-            s[k] += cartesian_sum(q, rows=all_classes, cols=(k, ))
-            s[k] -= q[k, k]
         
+        # compute the sum of the q(l, r) values, i.e. the mutual information I(D,E)
+        # I = sum(q.values())
+
+        # print("I(D, E):", I)
 
         # for each pair a, b, compute the I(D, E) after the potential merge of a and b
         # and pick the pair with the highest I(D, E)
         max_I = float("-inf")
         best_pair = None
         for a, b in itertools.combinations(classes, 2):
-            new_I = I - sub(s, q, a, b) + add(bigrams, p_l, p_r, all_classes, a, b)
+            new_I = I - L_get(L, a, b)
             if new_I > max_I:
                 max_I = new_I
                 best_pair = (a, b)
 
-        # merge classes
-        print("Merging:", best_pair)
-        print("max I(D, E):", max_I)
         a, b = best_pair
+
+        out_str = f"|{len(classes)}. I(D, E): {I}, loss: {I - max_I}, merging:\n{class_to_str(a)}\n{class_to_str(b)}\n"
+        print_and_write(out_str)
+
+
+        # merge classes
+        I = max_I
+
         #print("Merging", a, "and", b, "with L", L[min_pair])
-        classes.remove(a)
-        classes.remove(b)
+        new_classes = update_classes(classes, a, b)
+        new_all_classes = update_classes(all_classes, a, b)
 
-        new_class = merge(a, b)
+        update_counts(a, b, all_classes)
 
-        new_bigrams = defaultdict(lambda: 0)
+        q2 = update_q(q, a, b, all_classes)
 
-        for bigram, prob in bigrams.items():
-            # throw away bigrams containing a or b
-            l, r = bigram
+        s2 = update_s(s, q, q2, a, b, all_classes)
 
-            if l == a or l == b or r == a or r == b:
-                continue
+        # classes == considerer classes (not all classes)
+        L2 = update_L(L, q, q2, s, s2, a, b, classes, new_all_classes)
 
-            new_bigrams[l, r] = prob
+        q = q2
+        s = s2
+        L = L2
+        classes = new_classes
+        all_classes = new_all_classes
 
-        # add the new class to the bigrams
-        for c in all_bigram_classes(bigrams):
-            if c == a or c == b:
-                print("continuing")
-                continue
-            new_bigrams[new_class, c] = cartesian_sum(bigrams, rows=(a, b), cols=(c, ))
-            new_bigrams[c, new_class] = cartesian_sum(bigrams, rows=(c, ), cols=(a, b))
-        
-        new_bigrams[new_class, new_class] = cartesian_sum(bigrams, rows=(a, b), cols=(a, b))
-        bigrams = new_bigrams
+    print_and_write(f"\n\nFinal {len(classes)} classes:")
 
-        # copy old bigram
-        # new_bigrams = {}
-        # for bigram, prob in bigrams.items():
-        #     l, r = bigram
-        #     if l in classes and r in classes:
-        #         new_bigrams[l, r] = prob
+    for c in classes:
+        print_and_write(class_to_str(c))
 
-        # add new bigrams with the merged class
-
-
-        # update p
-        p_l = init_unigram_probs(new_bigrams, left=True)
-        p_r = init_unigram_probs(new_bigrams, left=False)
-
-        # add new merged class to the set of classes
-        classes.add(new_class)
-
-
+    output_file.close()
 
     return classes
 
 
-def print_classes(classes):
-    for c in classes:
-        print(" ".join(c.split("/")))
+def class_to_str(cl):
+    # return " ".join(sorted(cl.split("/")))
+    return " ".join(cl.split("/"))
 
 
 if __name__ == '__main__':
-    print("started")
+    import argparse
 
-    path = "datasets/TEXTCZ1.ptg"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--N", type=int, default=None, help="Dataset cutoff size")
+    parser.add_argument("--target_num_classes", type=int, default=15, help="Stop merging when this many classes are reached")
+    parser.add_argument("--dataset_path", type=str, default="datasets/TEXTEN1.ptg")
+
+    parser.add_argument("--mode", type=str, choices=["words", "tags"], default="words", help="Whether to use words or tags as classes to be merged")
+
+
+    args = parser.parse_args()
+
+    if args.mode == "words":
+        min_word_count = 10
+    else:
+        min_word_count = 5
+
+
+    if args.dataset_path.endswith("EN1.ptg"):
+        lang = "EN"
+    elif args.dataset_path.endswith("CZ1.ptg"):
+        lang = "CZ"
+    else:
+        raise ValueError("Unknown dataset")
+
+
+
     print("loading data")
-    words, tags = load_data(path)
+    words, tags = load_data(args.dataset_path)
 
-    print("Number of unique words:", len(set(words)))
+    data = words if args.mode == "words" else tags
 
-    print("computing word classes, stopping at 15 classes")
-    classes = compute_word_classes(words, target_num_classes=15, min_word_count=10, dataset_cutoff=8000)
-
-    print_classes(classes)
+    print("Number of unique datapoints:", len(set(data)))
 
 
+    if args.N is None or args.N > len(data):
+        args.N = len(data)
 
-    print("getting unigram counts")
-    uni = get_unigram_counts(words)
-    print(uni["WHEN"])
+    results_fn = f"results/merge_results_{args.mode}_{lang}_N-{args.N}_target_num-{args.target_num_classes}.txt"
 
+    classes = compute_word_classes(data, target_num_classes=args.target_num_classes, output_fn=results_fn, min_word_count=min_word_count, N=args.N)
 
